4373c9b4947535b6d1956407536370a3bc03e5e3
marcosjoseph <marquijoseph@gmail.com>
2023-07-27 19:24:28 -0300

    Corrección While

--- main.js
+++ main.js
@@ -8,20 +8,39 @@ const Madera= function (nombre, cantidad, largo, ancho, grosor){
     this.ancho = ancho;
     this.grosor = grosor;
     this.pies = parseFloat((cantidad * (largo/pie) * (ancho/pulgada) * (grosor/pie)).toFixed(2));
+
+    if (isNaN(this.pies)) {
+        alert("Por favor, ingresá valores numéricos")
+    }
 }
 
-let maderas = [];
+let maderas = []
 
 alert ("Bienvenido al calculador de Pies de Madera!")
 
-let madera1 = new Madera (
-    prompt("Por favor ingresa el nombre de la madera"),
-    parseFloat(prompt("ingresá la cantidad de listones que necesitas")),
-    parseFloat(prompt("ingresá el largo en cm")),
-    parseFloat(prompt("ingresá el ancho en cm")),
-    parseFloat(prompt("ingresá el grosor en cm"))
-);
+let calcularOtraMadera = true;
+
+while (calcularOtraMadera) {
+
+    let madera = new Madera (
+        prompt("Por favor ingresa el nombre de la madera"),
+        parseFloat(prompt("ingresá la cantidad de listones que necesitas")),
+        parseFloat(prompt("ingresá el largo en cm")),
+        parseFloat(prompt("ingresá el ancho en cm")),
+        parseFloat(prompt("ingresá el grosor en cm"))
+    );
+
+    maderas.push(madera);
+
+    let respuesta = confirm("¿Querés calcular otra madera?")
+
+    if (respuesta==false) {
+            break}
+
+}
 
 console.table(maderas);
+
+alert("Muchas gracias!")
 
 
