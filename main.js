8acfa11a86c37e7e7697f22234f88dbbbffe3239
marcosjoseph <marquijoseph@gmail.com>
2023-07-29 10:45:56 -0300

    Filtro Consulta Madera

--- main.js
+++ main.js
@@ -11,37 +11,46 @@ const Madera= function (nombre, cantidad, largo, ancho, grosor){
 }
 
 let maderas = []
-let maderaCalculada = maderas.find((el) => el.nombre); 
-let totalPies = maderas.find((los) => los.pies)
 
 alert ("Bienvenido al calculador de Pies de Madera!")
 
-let calcularOtraMadera = true;
+let calcularMadera = true;
 
-while (calcularOtraMadera) {
+while (calcularMadera) {
+    let nombre = prompt("Por favor ingresa el nombre de la madera")
+    let cantidad = parseFloat(prompt("ingresá la cantidad de listones que necesitas"))
+    let largo = parseFloat(prompt("ingresá el largo en cm"))
+    let ancho = parseFloat(prompt("ingresá el ancho en cm"))
+    let grosor = parseFloat(prompt("ingresá el grosor en cm"))
 
-    let madera = new Madera (
-        prompt("Por favor ingresa el nombre de la madera"),
-        parseFloat(prompt("ingresá la cantidad de listones que necesitas")),
-        parseFloat(prompt("ingresá el largo en cm")),
-        parseFloat(prompt("ingresá el ancho en cm")),
-        parseFloat(prompt("ingresá el grosor en cm"))
-    );
+    let madera = new Madera (nombre, cantidad, largo, ancho, grosor);
 
-    if (isNaN(madera.cantidad)||isNaN(madera.largo)||isNaN(madera.ancho||isNaN(madera.grosor))) {
+    if (isNaN(cantidad)||isNaN(largo)||isNaN(ancho)||isNaN(grosor)) {
         alert("Por favor, ingresá valores numéricos")
     } 
     else {
         maderas.push(madera)
-        alert(`La cantidad de ${maderaCalculada} que necesitás, son ${totalPies} pies.`)    
+        alert(`La cantidad de ${madera.nombre} que necesitás, son ${madera.pies} pies.`)    
     }
 
-    let respuesta = confirm("¿Querés calcular otra madera?")
-
-    if (respuesta==false) {
+    let respuestaCalcularOtra = confirm("¿Querés calcular otra madera?")
+    if (respuestaCalcularOtra==false) {
             break}
-
 }
+
+let consultarMadera = true;
+
+while (consultarMadera) {
+    let respuestaConsultar = confirm("¿Querés consultar si ya has ingresado una madera?")
+    if (respuestaConsultar==false) {break};
+
+    let nombreConsultar = prompt("Ingresá el nombre de la madera a buscar")
+    if (maderas.find ((el) => el.nombre == nombreConsultar)) {
+        alert (`Ya has ingresado los valores de ${nombreConsultar}`)
+    } 
+    else {
+        alert (`No se ha ingresado ninguna madera con el nombre de ${nombreConsultar}`)}
+    } 
 
 console.table(maderas);
 
